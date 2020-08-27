64cd2e2ac7272a0014b9ca3aee2fec947c28beb8
Stanislav Bakryu <stasbakryu@gmail.com>
2020-08-27 16:56:15 +0300

    slider

--- src/js/scripts/function.js
+++ src/js/scripts/function.js
@@ -12,4 +12,11 @@ $(function () {
     asNavFor: '.header__slider',
 
   });
+  $('.serf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="">',
+
+  });
 });
