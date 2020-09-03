90e696da8212596f5847524fde98e94013d199bc
Stanislav Bakryu <stasbakryu@gmail.com>
2020-09-03 11:05:12 +0300

    last section

--- src/js/scripts/function.js
+++ src/js/scripts/function.js
@@ -4,9 +4,9 @@ $(function () {
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="">',
-    asNavFor: '.slider__dots'
+    asNavFor: '.slider__dotshead'
   });
-  $('.slider__dots').slick({
+  $('.slider__dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.header__slider',
@@ -17,6 +17,19 @@ $(function () {
     slidesToScroll: 1,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="">',
-
+    asNavFor: '.slider-map',
+  });
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.serf-slider',
+    focusOnSelect: true,
+  })
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="">',
   });
 });
